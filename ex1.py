864743340e1c29850d72a470bec031bb7bb263dc
Hackbright Student <info@hackbrightacademy.com>
2014-02-11 12:31:53 -0800

    It does all the things. Looping through files in original files directory, moves each to the appropriate alphabetical directory.

--- ex1.py
+++ ex1.py
@@ -1,5 +1,7 @@
 from os import mkdir
 from os import listdir
+from shutil import move
+
 # create 26 directories in the current dir, one for each letter of the alphabet
 # loop through files in ./original_files, organize into previously created dir
 # by alpha
@@ -11,3 +13,9 @@ currdir = listdir(".")
 for name in [chr(x) for x in range(97,123) if chr(x) not in currdir]:
     mkdir(name)
 
+original_files = listdir("./original_files")
+
+# Loop through files in original_files
+for filename in original_files:
+# Move it into the appropriate directory
+    move("./original_files/"+filename,"./"+filename[0]+"/.")
